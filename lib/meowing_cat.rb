e9ae1f3ecfdb06b74ca6a5595ce65f5951326019
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-19 17:10:40 +0300

    using attr_accessor

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name , :color
+    def initialize (name, color)
+       @name = name
+       @color = color
+    end 
+end
+
+Cat4 = Cat.new("rose", "white and brown")
+puts "the cat name is #{Cat4.name} and has a #{Cat4.color} color  "
+
+Cat4.name = "brown"
+Cat4.color = "black and white"
+
+puts "the cat name is #{Cat4.name} and has a #{Cat4.color} color  "
